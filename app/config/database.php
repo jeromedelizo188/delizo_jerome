cc612186ed775a139d15011b299098e520e21d4e
jeromedelizo188 <jeromedelizo9@gmail.com>
2025-09-17 07:30:17 +0800

    Update project

--- app/config/database.php
+++ app/config/database.php
@@ -70,15 +70,13 @@ $database['main'] = array(
 
 $database['main'] = array(
     'driver'	=> 'mysql',
-    'hostname'	=> 'localhost',
+    'hostname'	=> 'sql12.freesqldatabase.com',
     'port'		=> '3306',
-    'username'	=> 'root',
-    'password'	=> '',
-    'database'	=> 'mockdata',
+    'username'	=> 'sql12798924',
+    'password'	=> 'KKcnsjUdnF',
+    'database'	=> 'sql12798924',
     'charset'	=> 'utf8mb4',
-    'dbprefix'	=> '',
-    // Optional for SQLite
-    'path'      => ''
+    'dbprefix'	=> ''
 );
 
 ?>
