72fef6e2593d5bdc2907386fab578e5ed3d13a32
jeromedelizo188 <jeromedelizo9@gmail.com>
2025-09-17 10:35:23 +0800

    Last Commit

--- app/config/routes.php
+++ app/config/routes.php
@@ -43,7 +43,7 @@ defined('PREVENT_DIRECT_ACCESS') OR exit('No direct script access allowed');
 |
 */
 
-//$router->get('/', 'Welcome::index');
+$router->get('/','StudentsController::get_all', 'GET|POST');
 //$router ->get('/', 'HomeController::landing_page');
 //$router ->get('get-id/{id}/{name}', 'Welcome::get_id');
 //$router ->get('/profile/{fname}/{lname}', 'Welcome::profile');
